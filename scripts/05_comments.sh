5e41bcc21868f1df083b8a8a4570f1cf1b60a0be
Jishnu Raghavan G <jishraghavanoff4@gmail.com>
2026-03-26 20:40:20 +0530

    Add comments example in 05_comments.sh

---
+++ scripts/05_comments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Single line comment example
+
+echo "This line will execute"
+
+: '
+This is a multi-line comment block
+It will not execute
+'
+
+echo "Comments help in understanding code"
